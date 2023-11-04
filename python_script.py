450c002c6907081d76808ccefd7e5795e9cad66f
putripus <puspitaputri456@gmail.com>
2023-11-04 16:00:25 +0700

    Tambahkan python_script.py

---
+++ python_script.py
@@ -0,0 +1,21 @@
+import random
+
+
+game = True
+skor = 0
+
+while game == True:
+    bilangan1 = random.randrange(1,20)
+    bilangan2 = random.randrange(1,20)
+    print(f'\nBerapakah hasil perkalian berikut ini')
+    jawab = input(str(bilangan1) + " x " + str(bilangan2) + " = ")
+    jawaban_benar = bilangan1*bilangan2
+    if jawab == str(jawaban_benar):
+        skor += 10
+        print(f'Jawaban Anda Benar, Skor Anda = {skor}')
+    else:
+        print(f'Jawaban anda salah, jawaban seharusnya adalah {jawaban_benar}')
+    konfirmasi = input('\nApakah ingin melanjutkan game (Y/T)?')
+    if konfirmasi == 'T':
+        break
+print('Akhiri Game\n')
